5fcf0cd2aed13473854c62e285f2877b23a7e2bd
rozolins9 <103949057+rozolins9@users.noreply.github.com>
2022-05-11 18:26:46 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const name  = ['Latviešu', 'Angļu', 'Krievu'];
+document.getElementById("LV").innerHTML = name[0];
+document.getElementById("EN").innerHTML = name[1];
+document.getElementById("RU").innerHTML = name[2];
+
+function LV() {
+        document.getElementById("text").innerHTML = 'Sveiks!';
+}
+function EN() {
+    document.getElementById("text").innerHTML = 'Hello!';
+}
+function RU() {
+    document.getElementById("text").innerHTML = 'Privet!';
+}
+
+
+
+// Viss strādā :)
+// ideāli
+//super
+// Ričard uztaisīsi lūdzu branch Github?
+// 
+// Ok, ritdien, deres?
+// https://github.com/DCHTS/DCHTS
+//https://github.com/VladislavsHH
